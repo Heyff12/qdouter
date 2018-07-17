1b7879d539d1cec1d74fb450412c4f5bcb6e196c
chujunfang@qfpay.com <chujunfang@qfpay.com>
2018-07-17 12:05:07 +0800

    全选，取消传递ID

--- src/pages/merchant/sonlist.vue
+++ src/pages/merchant/sonlist.vue
@@ -100,7 +100,8 @@ export default {
       //   });
       //   return false;
       // }
-      let url = this.down_url + "?big_userid="+ this.pageId +"&mchnt_uids=" + this.product;
+      // let url = this.down_url + "?big_userid="+ this.pageId +"&mchnt_uids=" + this.product;
+      let url = this.down_url + "?big_userid="+ this.pageId;
       window.open(url);
     },
     //根据style判断是否获取信息
